7e96e3e26d6666abb93f2e72a0ac57887c21dd2a
Sam <7070683+santiq@users.noreply.github.com>
2021-10-12 13:16:06 -0300

    Added map id

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const memoryjs = require('memoryjs');
 
-
 async function getProcessInfo() {
     const processName = "D2R.exe";
     return new Promise((resolve, reject) => {
@@ -27,63 +26,73 @@ async function readMemoryBuffer (handle, address, size) {
     })
 }
 
-async function main() {
-    // Get D2 Process info
-    const processInfo = await getProcessInfo();
-
-    const handle = processInfo.handle;
-    const startingAddress = processInfo.modBaseAddr + 0x2055E40;
-    
-    // Create a buffer and store data on it
+async function getMapSeed(handle, startingAddress) {
     const buf = await readMemoryBuffer(handle, startingAddress, 8);
 
-
     const playerUnit = Number(buf.readBigUInt64LE(0));
-    const pPlayer = playerUnit + 0x10;
     
     const pAct = playerUnit + 0x20;
-
-    // Get the actual player memory address
-    const newBuf = await readMemoryBuffer(handle, pPlayer, 8);
-
-    const playerAddress = Number(newBuf.readBigUInt64LE(0));
-    const playerNameBuff = await readMemoryBuffer(handle, playerAddress, 16);
-
-    const playerName = playerNameBuff.toString();
-    
-    console.log("Player name: ", playerName)
-    
 
     const _buff1 = await readMemoryBuffer(handle, pAct, 8);
     const actAddress = Number(_buff1.readBigUInt64LE(0));
 
     const mapSeedAddress = actAddress + 0x14;
 
-    const pPathAddress = playerUnit + 0x38;
-
-    const _buff2 = await readMemoryBuffer(handle, pPathAddress, 8);
-
-    const path = Number(_buff2.readBigUInt64LE(0));
-
-    const posXAddress = path + 0x02;
-    const posYAddress = path + 0x06;
-
     const _buff3 = await readMemoryBuffer(handle, mapSeedAddress, 8);
-    
-    
-    const actIdAddress = playerUnit + 0x18;
-    const _buff6 = await readMemoryBuffer(handle, actIdAddress, 8); // 6 
-    console.log("Act id address" + actIdAddress.toString(16));
-    console.log("Act ID " + _buff6.readUInt32LE());
 
-    const _buff4 = await readMemoryBuffer(handle, posXAddress, 8);
-    console.log("X Pos ADDRESS : " + posXAddress.toString(16));
-
-    const _buff5 = await readMemoryBuffer(handle, posYAddress, 8);
-    
-    const message = 'Map Seed: ' + _buff3.readUInt32LE() + ' - X Pos: ' + _buff4.readUInt16LE() + ' - Y Pos: ' + _buff5.readUInt16LE();
-    console.log(message);
-
+    return _buff3.readUInt32LE();
 }
 
-setInterval(main, 1000)
+
+async function getCurrentMapId(handle, startingAddress) {
+    const _buffPlayerUnit = await readMemoryBuffer(handle, startingAddress, 8);
+    const pPlayerUnit = Number(_buffPlayerUnit.readBigUInt64LE(0));
+
+    const pPathAddress = pPlayerUnit + 0x38;
+
+    const _buffPath = await readMemoryBuffer(handle, pPathAddress, 8);
+
+    const pPath = Number(_buffPath.readBigUInt64LE(0))
+
+    const pRoom1 = pPath + 0x20;
+
+    const _buffRoom1 = await readMemoryBuffer(handle, pRoom1, 8);
+
+    const pRoom1Address = Number(_buffRoom1.readBigUInt64LE(0));
+
+    const pRoom2 = pRoom1Address + 0x18;
+
+    const _buffRoom2 = await readMemoryBuffer(handle, pRoom2, 8);
+
+    const pRoom2Address = Number(_buffRoom2.readBigUInt64LE(0));
+    
+    const pLevel = pRoom2Address + 0x90;
+
+    const _buffLevel = await readMemoryBuffer(handle, pLevel, 8);
+
+    const pLevelAddress = Number(_buffLevel.readBigUInt64LE(0));
+    
+    const dwLevelNo = pLevelAddress + 0x1F8;
+
+    const _buffLevelNo = await readMemoryBuffer(handle, dwLevelNo, 8);
+
+    return _buffLevelNo.readUInt32LE();
+}
+
+async function main() {
+    // Get D2 Process info
+    const processInfo = await getProcessInfo();
+
+    const handle = processInfo.handle;
+    const startingAddress = processInfo.modBaseAddr + 0x2055E40;
+
+    const mapId = await getCurrentMapId(handle, startingAddress);
+    const seedId = await getMapSeed(handle, startingAddress);
+    
+    console.log("Map seed: " + seedId);
+    console.log("Current level id: " + mapId);
+}
+
+const tickInterval = 1000 // milisecs
+
+setInterval(main, tickInterval)
